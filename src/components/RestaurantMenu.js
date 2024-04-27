1c38185511e9e137d4cbbd995a8cbf5f14f22783
Faizan Alam <faizanalam416@gmail.com>
2024-04-27 19:14:10 +0530

    menuChanges

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -17,15 +17,23 @@ const RestaurantMenu = () => {
 
     const restaurantImage = {IMG_CDN_URL} + resInfo?.cards[2]?.card?.card?.info?.cloudinaryImageId;
     const name = resInfo?.cards[2]?.card?.card?.info?.name;
+    const avgRating = resInfo?.cards[2]?.card?.card?.info?.avgRating;
     const cuisines = resInfo?.cards[2]?.card?.card?.info?.cuisines;
     const costForTwoMessage = resInfo?.cards[2]?.card?.card?.info?.costForTwoMessage;
+    const locality = resInfo?.cards[2]?.card?.card?.info?.locality;
+    const totalRatingsString = resInfo?.cards[2]?.card?.card?.info?.totalRatingsString;
 
     return (
-        <div className="p-2">
-            <img src={restaurantImage} alt={name}/>
-            <h1 className="font-bold">{name}</h1>
-            <p>{cuisines.join(", ")} - {costForTwoMessage}
-            </p>
+        <div className="w-[80%] mx-auto my-24">
+            <h1 className="font-bold text-2xl">{name}</h1>
+            <div className="flex w-[80%] mx-auto justify-between my-2 p-2 border-2">
+                <img src={restaurantImage} alt={name}/>
+                <div className="font-bold">
+                    <span className="bg-green-500 w-14 rounded-lg p-1">{avgRating}⭐</span> ({totalRatingsString})  . {costForTwoMessage}
+                    <p className="text-orange-700">{cuisines.join(", ")}</p>
+                    <p>Outlet : {locality}</p>
+                </div>
+            </div>
         </div>
       )
 }
